be8eeaf2e634c2f68366f82669a87ceec56bba11
Harpushpam Singh <ssingh.harpushpam@gmail.com>
2019-02-01 19:33:16 -0500

    First Commit

--- C0732134_Week04/Program.cs
+++ C0732134_Week04/Program.cs
@@ -6,7 +6,35 @@ namespace C0732134_Week04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Countryside c = new Countryside();
+        }
+    }
+
+    class Village
+    {
+        public bool isAstrilde = false;
+        public Village nextVillage;
+        public Village prevVillage;
+        public string villageName;
+    }
+
+    class Countryside
+    {
+        Village Toronto, Ajax, Maple;
+
+        public void run()
+        {
+            Maple = new Village();
+            Maple.villageName = "Maple";
+            Toronto = new Village();
+            Toronto.villageName = "Toronto";
+            Ajax = new Village();
+            Ajax.villageName = "Ajax";
+
+            Maple.nextVillage = Toronto;
+            Toronto.nextVillage = Ajax;
+            Ajax.nextVillage = Maple;
         }
     }
 }
+
